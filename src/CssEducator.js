28ce906a7e25e7772411ae3625c5c64c801a5457
dusanm0403 <dusan.marinic@netcast.rs>
2020-02-04 11:38:09 +0100

    css educator upload

--- src/CssEducator.js
+++ src/CssEducator.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import './CssEducator.css';
+import React, { useState } from 'react'
+import './CssEducator.css'
 
 import Slider from './components/slider'
+// data --- import slides content //
 import data from './data'
 import arrow from './assets/images/icons/arrow.svg'
-import cx from 'classnames'
+import cx from 'classnames' // library for switching element classes based on current state//
 
 const CssEducator = () => {
 
-  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0) // initial state //
 
+  // function handle arrow clicks //
   const handleSlideChange = (direction) => {
     if (direction === 'next') {
       setActiveIndex(activeIndex === data.slides.length - 1 ? 0 : activeIndex + 1)
@@ -18,6 +20,7 @@ const CssEducator = () => {
     }
   }
 
+  // variable for slider transform calucations //
   const translateY = `${100 * activeIndex}vh`
   const isOnStart = activeIndex === 0
   const isOnEnd = activeIndex === data.slides.length - 1
@@ -53,4 +56,4 @@ const CssEducator = () => {
   );
 }
 
-export default CssEducator;
+export default CssEducator
